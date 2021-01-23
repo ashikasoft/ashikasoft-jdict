28f86d1ba42644e7fc9d3d18e8e5861ab385f0ca
kbsant <keansant@gmail.com>
2021-01-23 16:22:55 +0900

    wip async loading

--- src/cljs/ashikasoft/jdict.cljs
+++ src/cljs/ashikasoft/jdict.cljs
@@ -1,24 +1,22 @@
 (ns ashikasoft.jdict
   (:require
-   [ajax.core :refer [GET POST]]
+   [ajax.core :refer [GET]]
    [ashikasoft.jdict-common :as common]
    [clojure.string :as string]))
 
 
 (defn load-lines-handler [read-lines-fn body]
   (let [lines (string/split body #"\n")]
-    (into [] (map read-fn) lines)))
+    (into [] (map read-lines-fn) lines)))
 
 (defn async-loader [error-fn store-fn read-lines-fn url]
-  (ajax/GET url {:handler #(store-fn (load-lines-handler read-lines-fn %))
+  (GET url {:handler #(store-fn (load-lines-handler read-lines-fn %))
                  :error-handler error-fn}))
 
 (defn async-state-loader [state store-keys read-lines-fn dir file]
   (let [error-fn #(swap! state assoc :error %)
-        store-fn #(swap! state assoc-in store-keys %1)
-        loader (fn [keyvec read-fn url]
-                 (async-loader error-fn (partial store-fn keyvec) read-fn url))])
-  (async-loader error-fn store-fn read-lines-fn (str dir "/" file)))
+        store-fn #(swap! state assoc-in store-keys %1)]
+    (async-loader error-fn store-fn read-lines-fn (str dir "/" file))))
 
 (defn async-create-dict
   "Create a dictionary instance using the given data resource loader.
@@ -33,12 +31,12 @@
   (let [state-loader (fn [store-keys read-fn file]
                            (async-state-loader state store-keys read-fn dir file))]
     (swap! state assoc :state-loader state-loader)
-    (load-kana-map (partial state-loader [:dict :kana-map]))
-    (load-index (partial state-loader [:dict :en-wnet]) "wnet_ej")
-    (load-index (partial state-loader [:dict :en-wnet]) "wnet_ej")
-    (load-index (partial state-loader [:dict :en-jmdict]) "eng_list")
-    (load-index (partial state-loader [:dict :kana-jmdict]) "kana_list")
-    (load-index (partial state-loader [:dict :kanji-jmdict]) "kanji_list")))
+    (common/load-kana-map (partial state-loader [:dict :kana-map]))
+    (common/load-index (partial state-loader [:dict :en-wnet]) "wnet_ej")
+    (common/load-index (partial state-loader [:dict :en-wnet]) "wnet_ej")
+    (common/load-index (partial state-loader [:dict :en-jmdict]) "eng_list")
+    (common/load-index (partial state-loader [:dict :kana-jmdict]) "kana_list")
+    (common/load-index (partial state-loader [:dict :kanji-jmdict]) "kanji_list")))
 
 (defn async-lookup
   "Look up a word from the dictionary.
@@ -46,16 +44,16 @@
   [dict word]
   (when-not (string/blank? word)
     (let [word (string/lower-case (string/trim word))
-          hiragana (to-hiragana dict word)
-          katakana (to-katakana dict word)]
+          hiragana (common/to-hiragana dict word)
+          katakana (common/to-katakana dict word)]
       (into []
         (concat
-          (when (roman? word) (lookup-wnet dict word))
+          (when (common/roman? word) (common/lookup-wnet dict word))
           (into (sorted-set) ;; JMDict entries should be de-duplicated
             (concat
-              (if (roman? word)
-                (lookup-jmdict dict word :en-jmdict)
-                (lookup-jmdict dict word :kana-jmdict :kanji-jmdict))
-              (when hiragana (lookup-jmdict dict hiragana :kana-jmdict :kanji-jmdict))
+              (if (common/roman? word)
+                (common/lookup-jmdict dict word :en-jmdict)
+                (common/lookup-jmdict dict word :kana-jmdict :kanji-jmdict))
+              (when hiragana (common/lookup-jmdict dict hiragana :kana-jmdict :kanji-jmdict))
               (when (and katakana (not= katakana hiragana))
-                (lookup-jmdict dict katakana :kana-jmdict :kanji-jmdict)))))))))
+                (common/lookup-jmdict dict katakana :kana-jmdict :kanji-jmdict)))))))))
--- test/cljc/ashikasoft/jdict_common_test.cljc
+++ test/cljc/ashikasoft/jdict_common_test.cljc
@@ -3,7 +3,7 @@
    [clojure.string :as string]
    [clojure.test :refer [deftest is testing]]
    [ashikasoft.test-resources :as resources]
-   [ashikasoft.jdict-common :refer :all]))
+   [ashikasoft.jdict-common :as common]))
 
 
 (defn res-loader [load-fn filename]
@@ -14,37 +14,37 @@
 
 (deftest test-roman?
   (testing "Anything with roman characters or numerals is roman."
-    (is (roman? "Parisian")))
+    (is (common/roman? "Parisian")))
   (testing "If there are no roman characters, it isn't roman."
-    (is (not (roman? "日本人")))))
+    (is (not (common/roman? "日本人")))))
 
 (deftest test-kana-conversion
-  (let [dict {:kana-map (load-kana-map res-loader)}]
+  (let [dict {:kana-map (common/load-kana-map res-loader)}]
     (testing "Given a resource loader, build a kana map."
       (is (= #{:hk :rk :rh :kh}
              (->> dict :kana-map keys (into #{})))))
     (testing "Translate a roman word into hiragana."
       (is (= "おにぎり を たべちゃった"
-             (to-hiragana dict "onigiri wo tabechatta"))))
+             (common/to-hiragana dict "onigiri wo tabechatta"))))
     (testing "Translate a roman word into katakana."
       (is (= "インターナショナル スクール"
-             (to-katakana dict "inta-nashonaru suku-ru"))))))
+             (common/to-katakana dict "inta-nashonaru suku-ru"))))))
 
 (deftest test-lookup-single-dictionary
   (testing "Loading a top-level index returna a map of lead entries to sub files."
-    (let [top-index (load-index res-loader "test_dict")
+    (let [top-index (common/load-index res-loader "test_dict")
           test-dict {:res-loader res-loader
                      :test-dict top-index}]
       (is (= ["aa" "bb" "cc"] (keys top-index)))
       (testing "Searching an exact match returns a result."
         (let [word "bc"
-              result (lookup-subfile-entries test-dict word [:test-dict] nil)]
+              result (common/lookup-subfile-entries test-dict word [:test-dict] nil)]
           (is (= ["bc#ビーシー"] result))))
       (testing "Searching an partial match returns multiple results."
         (let [word "b"
-              result (lookup-subfile-entries test-dict word [:test-dict] nil)]
+              result (common/lookup-subfile-entries test-dict word [:test-dict] nil)]
           (is (= ["bb#ババ" "bc#ビーシー"] result))))
       (testing "Searching a missing term returns an empty result."
         (let [word "x"
-              result (lookup-subfile-entries test-dict word [:test-dict] nil)]
+              result (common/lookup-subfile-entries test-dict word [:test-dict] nil)]
           (is (= [] result)))))))
