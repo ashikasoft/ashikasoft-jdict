05b6044562d269cde62d06f0da4371d0407c936d
kbsant <keansant@gmail.com>
2021-01-28 18:44:50 +0900

    bump deps

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://www.ashikasoft.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.2"]
                  [org.clojure/data.avl "0.1.0"]]
   :source-paths ["src/clj"
                  "src/cljc"
