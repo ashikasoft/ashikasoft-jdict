2ecaaec8505c20a3992e481a9d59f5a96307f3f1
kbsant <keansant@gmail.com>
2021-01-30 08:21:46 +0900

    bump snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ashikasoft/jdict "1.0.2"
+(defproject ashikasoft/jdict "1.0.3-SNAPSHOT"
   :description "Clojure implementation of Ashikasoft Japanese Dictionary"
   :url "http://www.ashikasoft.com"
   :license {:name "Eclipse Public License"
